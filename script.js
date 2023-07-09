8eb0b2f211f239f2ad4770e8346e5797c5cb4037
karol82 <karol82@int.pl>
2023-07-09 10:04:48 +0200

    Poprawki, opisy

--- script.js
+++ script.js
@@ -1,82 +1,78 @@
-function getWeather() {
-    var cityInput = document.getElementById("cityInput").value;
+function pobierzPogode() {
+    var miasto = document.getElementById("miasto").value;
     var apiKey = "c1cce6ad1fe74b618542deaffa3a5637";
-    var currentWeatherUrl = "https://api.weatherbit.io/v2.0/current?key=" + apiKey + "&city=" + cityInput;
-    var forecastWeatherUrl = "https://api.weatherbit.io/v2.0/forecast/daily?key=" + apiKey + "&city=" + cityInput + "&days=6";
+    var obecnaPogodaUrl = "https://api.weatherbit.io/v2.0/current?key=" + apiKey + "&city=" + miasto;
+    var prognozaUrl = "https://api.weatherbit.io/v2.0/forecast/daily?key=" + apiKey + "&city=" + miasto + "&days=6";
 
 
-    // Wysłanie żądania do API dla aktualnej pogody
-    fetch(currentWeatherUrl)
+    fetch(obecnaPogodaUrl)  // Wysłanie żądania do API dla obecnej pogody
         .then(function(response) {
             return response.json();
         })
-        .then(function(data) {
-            // Wyświetlanie aktualnej pogody
-            displayCurrentWeather(data.data[0]);
+        .then(function(data) { // Wyświetlanie aktualnej pogody
+            wyswietlObecnaPogoda(data.data[0]);
         })
         .catch(function(error) {
             console.log(error);
         });
 
-    // Wysłanie żądania do API dla prognozy pogody
-    fetch(forecastWeatherUrl)
+    fetch(prognozaUrl) // Wysłanie żądania do API dla prognozy pogody
         .then(function(response) {
             return response.json();
         })
-        .then(function(data) {
-            // Wyświetlanie prognozy pogody
-            displayForecastWeather(data.data);
-            przywolaj_boga();
+        .then(function(data) { // Wyświetlanie prognozy pogody
+            wyswietlPrognoza(data.data);
+            przywolajBoga();  // dodaj obrazek
         })
         .catch(function(error) {
             console.log(error);
         });
 }
 
-function displayCurrentWeather(currentWeather) {
-    var currentWeatherDiv = document.getElementById("currentWeather");
-    var temperature = currentWeather.temp;
-    var description = currentWeather.weather.description;
+function wyswietlObecnaPogoda(obecnaPogoda) {
+    var obecnaPogodaDiv = document.getElementById("obecnaPogoda");
+    var temp = obecnaPogoda.temp;
+    var opis = obecnaPogoda.weather.description;
 
-    currentWeatherDiv.innerHTML = "<h2>Aktualna pogoda</h2>" +
-        "<h4><p>Temperatura: " + temperature + "°C</p>" +
-        "<p>Warunki: " + description + "</p></<h4>";
+    obecnaPogodaDiv.innerHTML = "<h2>Aktualna pogoda</h2>" +
+        "<h4><p>Temperatura: " + temp + "°C</p>" +
+        "<p>Warunki: " + opis + "</p></<h4>";
 }
 
-function displayForecastWeather(forecastWeather) {
-    var forecastWeatherDiv = document.getElementById("forecastWeather");
-    forecastWeatherDiv.innerHTML = "<h2>Prognoza pogody na kolejne dni</h2>";
+function wyswietlPrognoza(prognoza) {
+    var prognozaDiv = document.getElementById("prognoza");
+    prognozaDiv.innerHTML = "<h2>Prognoza pogody na kolejne dni</h2>";
 
-    var forecastTable = document.createElement("table");
-    forecastTable.classList.add("weather-table");
+    var prognozaTabela = document.createElement("table");
+    prognozaTabela.classList.add("weather-table");
 
     var row;
     var rowIndex = 0;
 
-    for (var i = 0; i < forecastWeather.length; i++) {
+    for (var i = 0; i < prognoza.length; i++) {
         if (i % 3 === 0) {
-            row = forecastTable.insertRow(rowIndex);
+            row = prognozaTabela.insertRow(rowIndex);
             rowIndex++;
         }
 
-        var forecast = forecastWeather[i];
-        var date = forecast.valid_date;
+        var forecast = prognoza[i];
+        var data = forecast.valid_date;
         var maxTemp = forecast.max_temp;
         var minTemp = forecast.min_temp;
-        var description = forecast.weather.description;
+        var opis = forecast.weather.description;
 
         var cell = row.insertCell();
-        cell.innerHTML = "<h3>" + date + "</h3>" +
+        cell.innerHTML = "<h3>" + data + "</h3>" +
             "<p>Maks. : " + maxTemp + "°C</p>" +
             "<p>Min. : " + minTemp + "°C</p>" +
-            "<p>" + description + "</p>";
+            "<p>" + opis + "</p>";
     }
-    forecastWeatherDiv.appendChild(forecastTable);
+    prognozaDiv.appendChild(prognozaTabela);
 
 }
-function przywolaj_boga() {
-    var bogaDiv = document.getElementById("bog");
-    bogaDiv.innerHTML = '<p2>Dnia udanego, życzę Ci</p2> <img src="css/god.webp" alt="Buk">';
+function przywolajBoga() {
+    var bogDiv = document.getElementById("bog");
+    bogDiv.innerHTML = '<p2>Dnia udanego, życzę Ci</p2> <img src="css/god.webp" alt="Buk">';
 }
 
 
